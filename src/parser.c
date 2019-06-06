788e0006039859573bc592e3d7c9c9e3a75442a6
Yoann FOREAU <yforeau@e3r7p6.42.fr>
2019-06-06 16:54:23 +0200

    Renvoyer 0 0 et quitter en cas d'erreur

--- src/parser.c
+++ src/parser.c
@@ -6,34 +6,34 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 12:12:35 by yforeau           #+#    #+#             */
-/*   Updated: 2019/06/06 16:16:00 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/06/06 16:50:40 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 #include "get_next_line.h"
 
-static int	exit_parser(t_filler *mst, char *err)
+static int	exit_parser(t_filler *mst)
 {
 	if (mst->board)
 		ft_parser_free_double_array((void **)mst->board);
 	if (mst->piece)
 		ft_parser_free_double_array((void **)mst->piece);
-	dprintf(2, "map error: %s\n", err);
+	ft_putstr("0 0\n");
 	return (1);
 }
 
 int			parser(t_filler *mst)
 {
 	if (!mst->player && ft_parser_get_player(mst))
-		return (exit_parser(mst, "get_player"));
+		return (exit_parser(mst));
 	if (ft_parser_get_board_dims(mst))
-		return (exit_parser(mst, "get_board_dims"));
+		return (exit_parser(mst));
 	if (ft_parser_get_board(mst))
-		return (exit_parser(mst, "get_board"));
+		return (exit_parser(mst));
 	if (ft_parser_get_piece_dims(mst))
-		return (exit_parser(mst, "get_piece_dims"));
+		return (exit_parser(mst));
 	if (ft_parser_get_piece(mst))
-		return (exit_parser(mst, "get_piece"));
+		return (exit_parser(mst));
 	return (0);
 }
