aad54379bff01efb9cd00a0515a8b1e1d370c2ef
Yoann FOREAU <yforeau@e3r7p6.42.fr>
2019-06-06 17:10:17 +0200

    Supprimer main 2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 09:41:15 by yforeau           #+#    #+#             */
-/*   Updated: 2019/06/06 16:11:14 by yforeau          ###   ########.fr       */
+/*   Updated: 2019/06/06 17:07:25 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,55 +40,3 @@ int	main(void)
 	}
 	return (0);
 }
-/*
-int	main(void)
-{
-	t_filler	master;
-	t_filler	*mst;
-
-	mst = &master;
-	ft_bzero((void *)mst, sizeof(t_filler));
-	if (!parser(mst))
-	{
-		printf("YAAAAAAAY!!!!!\n\n");
-		printf("bszx = %d\n", mst->bszx);
-		printf("bszy = %d\n", mst->bszy);
-		int	**board = mst->board;
-		while (*board)
-		{
-			for (int i = 0; i < mst->bszx; i++)
-			{
-				switch ((*board)[i])
-				{
-					case 1:
-						printf(C_RED"1 "C_RESET);
-						break ;
-					case 2:
-						printf(C_GREEN"2 "C_RESET);
-						break ;
-					default:
-						printf(C_MAGENTA"3 "C_RESET);
-						break ;
-				}
-			}
-			printf("\n");
-			++board;
-		}
-		printf("\npszx = %d\n", mst->pszx);
-		printf("pszy = %d\n", mst->pszy);
-		char	**piece = mst->piece;
-		while (*piece)
-		{
-			for (int i = 0; i < mst->pszx; i++)
-				printf("%c", (*piece)[i]);
-			printf("\n");
-			++piece;
-		}
-		printf("\nanswerx = %d\n", mst->answerx);
-		printf("answery = %d\n", mst->answery);
-		printf("mst->player = %s\n", mst->player == P_O ? "P_O" : "P_X");
-	}
-	else
-		printf("nay :(\n");
-	return (0);
-}*/
