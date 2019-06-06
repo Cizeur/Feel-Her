2bb9bef59493dabb0a4b7aa22cba737dc0b25237
Cizeur <cgiron@e3r7p7.42.fr>
2019-06-06 14:17:27 +0200

    premier algo de solve

---
+++ src/ft_solver_check_score.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_solver_check_score.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 10:37:23 by cgiron            #+#    #+#             */
+/*   Updated: 2019/06/06 10:37:40 by cgiron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "filler.h"
+
+int		ft_only_one_adversary(t_filler *mf, int i, int j)
+{
+	int cov_piece;
+	int	score;
+	int ii;
+	int jj;
+
+	score = 0;
+	cov_piece = 0;
+	jj = -1;
+	while ((ii = -1) && ++jj < mf->pszy)
+	{
+		while (++ii < mf->pszx)
+		{
+			if (mf->piece[jj][ii] != '*')
+				continue;
+			cov_piece += mf->board[j + jj][i + ii] == mf->player ? 1 : 0;
+			if (mf->board[j + jj][i + ii] == mf->adv || cov_piece > 1)
+				return (0);
+			score += mf->board[j + jj][i + ii];
+		}
+	}
+	if (cov_piece != 1 || (score <= mf->best_score && mf->best_score))
+		return (cov_piece == 1 ? 1 : 0);
+	mf->best_score = score;
+	mf->answerx = i;
+	mf->answery = j;
+	return (1);
+}
+
+
+void	ft_solver_check_score(t_filler *mf, int i, int j)
+{
+	if (i + mf->pszx > mf->bszx || j + mf->pszy > mf->bszy)
+		return;
+	if (!ft_only_one_adversary(mf, i, j))
+		return;
+	mf->board[j][i] = mf->board[j][i] < 0 ? 4 : mf->board[j][i];
+}                  
