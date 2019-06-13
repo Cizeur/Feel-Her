2586b78cc9693c2bbeb5b67392f26c710e35daae
Yoann FOREAU <yforeau@e3r6p3.42.fr>
2019-06-13 15:14:33 +0200

    Add blind option

--- resources/test.sh
+++ resources/test.sh
@@ -6,7 +6,7 @@
 #    By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/06/07 16:09:15 by cgiron            #+#    #+#              #
-#    Updated: 2019/06/13 14:53:20 by yforeau          ###   ########.fr        #
+#    Updated: 2019/06/13 15:14:21 by yforeau          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -33,7 +33,7 @@ i=0
 
 function usage
 {
-	printf "usage: test.sh p1 p2 [-n turns] [-m map] [-s width height] [-rkh]\n\n"
+	printf "usage: test.sh p1 p2 [-n turns] [-m map] [-s width height] [-rkhb]\n\n"
 	printf "	-h | --help:	Print this.\n"
 	printf "	-n | --turns:	Number of turns.\n"
 	printf "	-m | --map:	Give a map to 'test.sh'.\n"
@@ -43,6 +43,7 @@ function usage
 	printf "			directory. This option supersedes the\n"
 	printf "			given map, if any.\n"
 	printf "	-k | --keep:	Keep output in test_outputs.\n"
+	printf "	-b | --blind:	Do not prompt for visualisation.\n"
 }
 
 ############################# Options parser ###################################
@@ -76,6 +77,9 @@ do
 		-k | --keep )
 			KEEP_OUTPUT=1
 			;;
+		-b | --blind )
+			WATCH_PROMPT=0
+			;;
 		* )
 			if [ "$player1" == "" ]; then
 				player1=$1
@@ -93,17 +97,17 @@ mkdir -p test_outputs
 
 while [ $i -lt $n_turns ]
 do
-	if [ $i -lt 10 ]; then
+	if [ $((i+1)) -lt 10 ]; then
 		istr="0$((i+1))"
 	else
 		istr="$((i+1))"
 	fi
-	seed=$RANDOM
+	seed=$(($RANDOM%10000))
 	if [ $RAND_MAP -ne 0 ]; then
 		test_maps/a.out $RAND_MAP_SIZE > test_outputs/round_"$istr"_map
 		map=test_outputs/round_"$istr"_map
 	fi
-	echo "round" $istr/$n_turns
+	echo "round" $istr/$n_turns " - seed: " $seed
 	total=0
 	for j in 1 2
 	do
@@ -148,10 +152,12 @@ do
 	else
 		echo "no clear winer..."
 	fi
-	read -p "watch round ? (y/n) " answer
-	if [ "$answer" == "y" -o "$answer" == "Y" ]; then
-		cat test_outputs/round_"$istr"_output_a | $visualizer &> /dev/null &
-		cat test_outputs/round_"$istr"_output_b | $visualizer &> /dev/null &
+	if [ $WATCH_PROMPT -ne 0 ]; then
+		read -p "watch round ? (y/n) " answer
+		if [ "$answer" == "y" -o "$answer" == "Y" ]; then
+			cat test_outputs/round_"$istr"_output_a | $visualizer &> /dev/null &
+			cat test_outputs/round_"$istr"_output_b | $visualizer &> /dev/null &
+		fi
 	fi
 	if [ $KEEP_OUTPUT -eq 0 ]; then
 		rm test_outputs/round_"$istr"_output_a
