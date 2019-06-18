7e0f7f231cea3ea15f3c99c319a4cd712399de25
Cizeur <cgiron@e3r6p1.42.fr>
2019-06-18 08:35:29 +0200

    retrait usage sur generateur -- problematique pour les map invalides

--- resources/test_maps/map_generateur.c
+++ resources/test_maps/map_generateur.c
@@ -6,7 +6,7 @@
 /*   By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 11:45:30 by cgiron            #+#    #+#             */
-/*   Updated: 2019/06/04 11:54:23 by cgiron           ###   ########.fr       */
+/*   Updated: 2019/06/18 08:33:49 by cgiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,40 @@
 
 static int		random_int(int size)
 {
-	return(rand() % size);
+	return (rand() % size);
 }
 
-
-int				main(int ac, char **av)
+static void		print_map(int pos_o[2], int pos_x[2], int size_x, int size_y)
 {
 	int i;
 	int j;
+
+	j = -1;
+	while ((i = -1) && ++j < size_y)
+	{
+		while (++i < size_x)
+		{
+			if ((pos_o[0] == i && pos_o[1] == j))
+				printf("O");
+			else if ((pos_x[0] == i && pos_x[1] == j))
+				printf("X");
+			else
+				printf(".");
+		}
+		printf("\n");
+	}
+}
+
+int				main(int ac, char **av)
+{
 	int pos_o[2];
 	int pos_x[2];
 
-	srand(time(NULL) * 1000 + time(NULL)); 
+	srand(time(NULL) * 1000 + time(NULL));
 	ac--;
 	av++;
-	if (ac != 2 || atoi(av[0]) <= 0 || atoi(av[1]) <= 0)
-		printf ("./map_gen [ X Y ] (X et Y positif)\n");
+	if (ac != 2 || atoi(av[0]) <= 2 || atoi(av[1]) <= 2)
+		return(0);
 	else
 	{
 		pos_o[0] = random_int(atoi(av[0]));
@@ -43,20 +61,7 @@ int				main(int ac, char **av)
 			pos_x[0] = random_int(atoi(av[0]));
 			pos_x[1] = random_int(atoi(av[1]));
 		}
-		j = -1;
-		while ((i = -1) && ++j < atoi(av[1]))
-		{
-			while (++i < atoi(av[0]))
-			{
-				if ((pos_o[0] == i && pos_o[1] == j))
-			 		printf("O");
-				else if ((pos_x[0] == i && pos_x[1] == j))
-			 		printf("X");
-				else 
-			 		printf(".");
-			}
-			printf("\n");
-		}
+		print_map(pos_o, pos_x, atoi(av[0]), atoi(av[1]));
 	}
-
+	return(0);
 }
