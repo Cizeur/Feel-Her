5e72b8d140d995c1fcbd15bd17b7a27de020e835
Cizeur <cgiron@e3r1p12.42.fr>
2019-06-19 16:43:55 +0200

    massive push again - plus de listing de header dans le h generale inclusion des headers selon besoin - dependance complete sur les c les includes et les .mk

---
+++ shared_val.mk
@@ -0,0 +1,23 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    share_flags.mk                                     :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cgiron <cgiron@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/06/19 09:36:56 by cgiron            #+#    #+#              #
+#    Updated: 2019/06/19 16:40:51 by cgiron           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SHARE_SRC_DIR			:= src
+SHARE_OBJ_DIR			:= obj
+SHARE_INC_DIR			:= includes
+
+vpath %.c $(SHARE_SRC_DIR)
+vpath %.h $(SHARE_INC_DIR)
+vpath %.o $(SHARE_OBJ_DIR)
+
+SHARE_FLAGS				:= -Wall -Wextra -Werror -O3 -ffreestanding -fno-builtin
+SHARE_FLAGS				+= -I $(SHARE_INC_DIR) -flto
+SHARE_DEBUG				:= -O0 -g
